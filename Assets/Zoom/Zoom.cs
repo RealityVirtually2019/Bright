8976f35e2374f16c34745739c03f822daebc1084
Bright Dev Team <eld-ar@outlook.com>
2019-01-18 21:22:01 -0500

    Moved Script Folders to root of Project

---
+++ Assets/Zoom/Zoom.cs
@@ -0,0 +1,32 @@
+﻿// Starts the default camera and assigns the texture to the current renderer
+using UnityEngine;
+using System.Collections;
+
+public class Zoom : MonoBehaviour {
+  WebCamTexture webcamTexture;
+
+    void Start() {
+        Debug.Log("Initiating Zoom");
+
+        webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    void OnEnable() {
+        Debug.Log("Enabling Zoom");
+
+        if (webcamTexture && !webcamTexture.isPlaying)
+        {
+            webcamTexture.Play();
+        }
+        
+    }
+
+    void OnDisable() {
+        Debug.Log("Disabling Zoom");
+        
+        webcamTexture.Stop();
+    }
+}
