ee3b54acd387d77c791e4da01ef2fd33df1045b4
Jan Simson <jan.simson@gmx.de>
2019-01-19 09:43:14 -0500

    Added script to send SMS over twilio

---
+++ Assets/CallForHelp/TwilioSMS.cs
@@ -0,0 +1,42 @@
+﻿// Modified from https://github.com/wontonst/twilio-sms-unity
+
+using UnityEngine;
+using System.Collections;
+
+public class TwilioSMS : MonoBehaviour
+{
+  string url = "api.twilio.com/2010-04-01/Accounts/";
+  string service = "/Messages.json";
+  public string from;
+  public string to;
+  public string account_sid;
+  public string auth;
+
+  public void SendSMS(String body)
+  {
+    WWWForm form = new WWWForm();
+    form.AddField("To", to);
+    form.AddField("From", from);
+    //string bodyWithoutSpace = body.Replace (" ", "%20");//Twilio doesn't need this conversion
+    form.AddField("Body", body);
+    string completeurl = "https://" + account_sid + ":" + auth + "@" + url + account_sid + service;
+    Debug.Log(completeurl);
+    WWW www = new WWW(completeurl, form);
+    StartCoroutine(WaitForRequest(www));
+  }
+
+  IEnumerator WaitForRequest(WWW www)
+  {
+    yield return www;
+
+    // check for errors
+    if (www.error == null)
+    {
+      Debug.Log("WWW Ok! SMS sent through Web API: " + www.text);
+    }
+    else
+    {
+      Debug.Log("WWW Error: " + www.error);
+    }
+  }
+}
