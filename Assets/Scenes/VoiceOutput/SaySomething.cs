a9a208677f8d3c9c05a7a9427acf06098fcf2d3d
Bright Dev Team <eld-ar@outlook.com>
2019-01-18 20:23:58 -0500

    Added Test textToSpeech

---
+++ Assets/Scenes/VoiceOutput/SaySomething.cs
@@ -0,0 +1,20 @@
+﻿using HoloToolkit.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaySomething : MonoBehaviour
+{
+    public TextToSpeech textToSpeech;
+  
+    void Start()
+    {
+        textToSpeech.StartSpeaking("Hello RealitzVirtually 2019 yeah");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
