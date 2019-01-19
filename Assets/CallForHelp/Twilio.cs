97cc9bbaaa1cf8cecec17d75e8831a114b5c7fbc
Jan Simson <jan.simson@gmx.de>
2019-01-19 16:29:14 -0500

    Use UnityWebRequest for Twilio API Request
    Instead of WWW

--- Assets/CallForHelp/Twilio.cs
+++ Assets/CallForHelp/Twilio.cs
@@ -2,6 +2,7 @@
 
 using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
 public class Twilio : MonoBehaviour
 {
@@ -11,52 +12,87 @@ public class Twilio : MonoBehaviour
   public string from;
   public string to;
 
-  public void SendSMS(string body)
+  string authenticate()
   {
     // Retrieve auth information from JSON
-    string account_sid = Globals.instance.credentials.twilio_account_sid;
-    string auth = Globals.instance.credentials.twilio_auth;
+    string username = Globals.instance.credentials.twilio_account_sid;
+    string password = Globals.instance.credentials.twilio_auth;
+
+    string auth = username + ":" + password;
+    auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+    auth = "Basic " + auth;
+    return auth;
+  }
+
+  // The url has to point to an xml file containing instructions on what twilio should say
+  // If the file is hosted on github, changes will only take effect AFTER pushing changes there.
+  public void SendSMS(string url)
+  {
+    StartCoroutine(DoSendSMS(url));
+  }
+  IEnumerator DoSendSMS(string body)
+  {
+    string authorization = authenticate();
 
     WWWForm form = new WWWForm();
     form.AddField("To", to);
     form.AddField("From", from);
     form.AddField("Body", body);
-    string completeurl = "https://" + account_sid + ":" + auth + "@" + apiUrl + account_sid + smsService;
+
+    string completeurl = "https://" + apiUrl + Globals.instance.credentials.twilio_account_sid + smsService;
     Debug.Log(completeurl);
-    WWW www = new WWW(completeurl, form);
-    StartCoroutine(WaitForRequest(www));
+
+    using (UnityWebRequest www = UnityWebRequest.Post(completeurl, form))
+    {
+      www.SetRequestHeader("AUTHORIZATION", authorization);
+
+      yield return www.SendWebRequest();
+
+      if (www.isNetworkError || www.isHttpError)
+      {
+        Debug.Log(www.error);
+      }
+      else
+      {
+        // Show results as text
+        Debug.Log("Response" + www.downloadHandler.text);
+      }
+    }
   }
 
   // The url has to point to an xml file containing instructions on what twilio should say
   // If the file is hosted on github, changes will only take effect AFTER pushing changes there.
   public void MakeCall(string url)
   {
-    // Retrieve auth information from JSON
-    string account_sid = Globals.instance.credentials.twilio_account_sid;
-    string auth = Globals.instance.credentials.twilio_auth;
+    StartCoroutine(DoMakeCall(url));
+  }
+  IEnumerator DoMakeCall(string speechUrl)
+  {
+    string authorization = authenticate();
 
     WWWForm form = new WWWForm();
     form.AddField("To", to);
     form.AddField("From", from);
-    form.AddField("Url", url);
-    string completeurl = "https://" + account_sid + ":" + auth + "@" + apiUrl + account_sid + callService;
+    form.AddField("Url", speechUrl);
+
+    string completeurl = "https://" + apiUrl + Globals.instance.credentials.twilio_account_sid + callService;
     Debug.Log(completeurl);
-    WWW www = new WWW(completeurl, form);
-    StartCoroutine(WaitForRequest(www));
-  }
 
-  IEnumerator WaitForRequest(WWW www)
-  {
-    yield return www;
+    using (UnityWebRequest www = UnityWebRequest.Post(completeurl, form))
+    {
+      www.SetRequestHeader("AUTHORIZATION", authorization);
 
-    // check for errors
-    if (www.error == null)
-    {
-      Debug.Log("WWW Success, Response: " + www.text);
-    }
-    else
-    {
-      Debug.Log("WWW Error: " + www.error + " Response: " + www.text);
+      yield return www.SendWebRequest();
+
+      if (www.isNetworkError || www.isHttpError)
+      {
+        Debug.Log(www.error);
+      }
+      else
+      {
+        // Show results as text
+        Debug.Log("Response" + www.downloadHandler.text);
+      }
     }
   }
 }
