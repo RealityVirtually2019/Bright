d3c20a021e19990ff1a1702872bbf920c405030d
Bright Dev Team <eld-ar@outlook.com>
2019-01-20 09:14:12 -0500

    Added voice feedback to zooming

--- Assets/Zoom/ZoomManager.cs
+++ Assets/Zoom/ZoomManager.cs
@@ -10,26 +10,36 @@ public class ZoomManager : MonoBehaviour
     private float maxZoomFactor = 2.0f;
     private float minZoomFactor = 0.5f;
     private float zoomFactor = .5f;
+    public float zoomStep = .5f;
 
     public void tryZoomIn() {
         zoomPlane.SetActive(true);
 
         if (zoomFactor <= maxZoomFactor) {
-            zoomFactor += 0.25F;
+            Globals.instance.textToSpeech.StartSpeaking("Zooming in");
+            zoomFactor += zoomStep;
             ApplyZoom();
         } else {
             //can't zoom in more
+            Globals.instance.textToSpeech.StartSpeaking("Can't zoom in any more");
         }
     }
 
     public void zoomOut() {
         if (zoomFactor >= minZoomFactor) {
-            zoomFactor -= 0.25F;
+            Globals.instance.textToSpeech.StartSpeaking("Zooming out");
+            zoomFactor -= zoomStep;
             ApplyZoom();
         } else {
             // stop camera when smaller than the start size
-            zoomPlane.SetActive(false);
+            StopZoom();
         }
+    }
+
+    public void StopZoom()
+    {
+        Globals.instance.textToSpeech.StartSpeaking("Stopping Zoom");
+        zoomPlane.SetActive(false);
     }
 
     private void ApplyZoom() {
