53e0d68dde2c96e516434e7f60c888a44a6cc490
adias9 <andreasdias9@gmail.com>
2019-01-19 22:58:42 -0500

    add zoom manager

---
+++ Assets/Zoom/ZoomManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomManager : MonoBehaviour
+{
+    public GameObject zoomPlane;
+    public GameObject zoomObject;
+
+    private Vector3 maxScale = new Vector3(2.0F, 2.0F, 2.0F);
+    private Vector3 minScale = new Vector3(0.0F, 0.0F, 0.0F);
+
+    public void tryZoomIn() {
+        if (zoomObject.transform.localScale != maxScale) {
+            zoomObject.transform.localScale += new Vector3(0.25F, 0.25F, 0.25F);
+        } else {
+            //can't zoom in more
+        }
+    }
+
+    public void zoomOut() {
+        if (zoomObject.transform.localScale != minScale) {
+            zoomObject.transform.localScale -= new Vector3(0.25F, 0.25F, 0.25F);
+        } else {
+            // stop camera when smaller than the start size
+            zoomPlane.SetActive(false);
+        }
+    }
+}
